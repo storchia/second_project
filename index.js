ecec5870969edcc46b7610089edf5622e6b08623
Sergio Andres <sergiotorchia@hotmail.com>
2022-06-29 20:23:34 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const myName = 'Sergio Andres';
+
+const age = 41;
+
+const dev = true;
+
+const dob = new Date(1980, 8, 26);
+
+const book = {
+  title: 'Bestiario',
+  author: 'Cortazar',
+  year: new Date('1952'),
+  url: 'https://www.amazon.com/Bestiario-CORTAZAR-JULIO/dp/B003B0YMAK/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1656544322&sr=8-1'
+};
+
+console.log(myName, age, dev, dob, book);
